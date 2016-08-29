7fb2692fe3ef274ffafa6b4f3126fbebab737cea
maple <305554601@qq.com>
2016-08-29 16:41:39 +0800

    Initial Commit

---
+++ tableViewContainWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewContainWebView
+//
+//  Created by maple on 16/8/29.
+//  Copyright © 2016年 maple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
